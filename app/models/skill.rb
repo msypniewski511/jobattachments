ea9ac50a87a1271f0d656c057d9ac914932009ba
Maciej Sypniewski <msypniewski511@gmail.com>
2020-08-01 15:03:55 +0100

    heroku  complain  /app/app/models/skill.rb:10:in

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -8,5 +8,5 @@ class Skill
 
   # metal-kitchen-worktops", "id": 47, "order": "960", "crawlableInLocal": false, "count": 11 },"
   field :count, type: Integer
-  field :crawlableInLocal, type: Boolean, defult: false
+  field :crawlableInLocal, type: Boolean, default: false
 end
