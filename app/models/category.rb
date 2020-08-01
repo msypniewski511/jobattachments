008990ff6ab82aeaf9f85af113d08de31ede39a3
Maciej Sypniewski <msypniewski511@gmail.com>
2020-08-01 12:02:09 +0100

    Dependabot alerts and resolved unfinished category model issue

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category
   include Mongoid::Document
   include Mongoid::Timestamps
   # field :books
-  key :name, String
+  # key :name, String
   # has_and_belongs_to_many :books
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -6,7 +6,7 @@ class Skill
   field :name, type: String
   field :skillCode, type: String
 
-    metal-kitchen-worktops", "id": 47, "order": "960", "crawlableInLocal": false, "count": 11 },"
-    field :count, Type: Integer
-    field :crawlableInLocal, Type: Boolean, defult: false
+  # metal-kitchen-worktops", "id": 47, "order": "960", "crawlableInLocal": false, "count": 11 },"
+  field :count, Type: Integer
+  field :crawlableInLocal, Type: Boolean, defult: false
 end
