373221e30a07a56d1d51fda8f10f1081463fba01
Maciej Sypniewski <msypniewski511@gmail.com>
2020-09-15 22:08:02 +0100

    111111

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,171 +1,160 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import('@rails/ujs');
 
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
-import("@rails/ujs");
+import * as bg_sort from 'datatables.net-dt/images/sort_both.png';
 
-import * as bg_sort from 'datatables.net-dt/images/sort_both.png'
-
-require.context("../packs/assets/img", true);
+require.context('../packs/assets/img', true);
 require.context('../images', true);
-import "@fortawesome/fontawesome-free/js/all";
-import("@fortawesome/fontawesome-free");
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-library.add(fas, fab)
+import '@fortawesome/fontawesome-free/js/all';
+import('@fortawesome/fontawesome-free');
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fas, fab);
 
 global.$ = global.jQuery = jQuery;
 
-import "bootstrap";
-import "../packs/assets/js/core/bootstrap-material-design.min.js";
-import "moment";
-import("../packs/assets/js/plugins/bootstrap-datetimepicker");
+import 'bootstrap';
+import '../packs/assets/js/core/bootstrap-material-design.min.js';
+import 'moment';
+import('../packs/assets/js/plugins/bootstrap-datetimepicker');
 
-var noUiSlider = require("nouislider");
+var noUiSlider = require('nouislider');
 global.noUiSlider = noUiSlider;
-window.noUiSlider = noUiSlider
-import "select2"
-import "select2/dist/css/select2.css"
+window.noUiSlider = noUiSlider;
+import 'select2';
+import 'select2/dist/css/select2.css';
 import 'nouislider/distribute/nouislider.css';
-import "../src/plugins/jquery.sharrre";
+import '../src/plugins/jquery.sharrre';
 
 import('jquery');
-require("chart.js");
-import "../packs/scripts/custom";
+require('chart.js');
+import '../packs/scripts/custom';
 
 var dt = require('datatables.net-dt');
 
-import "../src/material-dashboard";
+import '../src/material-dashboard';
 
-import "../src/material-kit"
-import "base"
+import '../src/material-kit';
+import 'base';
 
-import "../demo/demo";
+import '../demo/demo';
 
 // es modules are recommended, if available, especially for typescript
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 import { initial } from 'lodash';
 // require("flatpickr/src/style/themes")
 
-
 import '../src/google_maps';
-import stickElement from '../../../node_modules/stick-element'
-
+import stickElement from '../../../node_modules/stick-element';
 
 import '../src/plugins/jquery.sharrre';
 
-
-
 $(document).ready(function () {
-  const dupa = 'dupa'
-  flatpickr('[data-behavior="flatpickr"]', {
-    altInput: true,
-    altFormat: "F j, Y",
-    dateFormat: "Y-m-d",
-    defaultDate: new Date(1960, 0, 10)
-  })
-  dt
-  $('#jobs_list_id').DataTable();
+	const dupa = 'dupa';
+	flatpickr('[data-behavior="flatpickr"]', {
+		altInput: true,
+		altFormat: 'F j, Y',
+		dateFormat: 'Y-m-d',
+		defaultDate: new Date(1960, 0, 10),
+	});
+	dt;
+	$('#jobs_list_id').DataTable();
 
-  $('.js-example-basic-multiple').select2();
+	$('.js-example-basic-multiple').select2();
 
+	// Google maps
 
-  // Google maps
-
-  let tmp = googleMap();
+	let tmp = googleMap();
 });
 $(document).on('turbolinks:load', () => {
-  console.log("sfsadfsadf")
+	console.log('sfsadfsadf');
 });
 
-const { postcodes } = require("@ideal-postcodes/api-fixtures");
+const { postcodes } = require('@ideal-postcodes/api-fixtures');
 
 // Example for postcodes->success
 
 $(document).ready(function () {
+	$('.toggle-submenu').on('click', function (e) {
+		// console.log(e);
+		$('.tab-content').toggleClass('hidden');
+	});
 
-  $('.toggle-submenu').on('click', function (e) {
-    // console.log(e);
-    $('.tab-content').toggleClass('hidden')
-  })
-
-  szukaj1();
-})
+	szukaj1();
+});
 
 import('@ideal-postcodes/api-fixtures');
 import('@ideal-postcodes/core-browser');
 
-var setupPostcodeLookup = require('../src/plugins/postcode')
+var setupPostcodeLookup = require('../src/plugins/postcode');
 function szukaj1() {
+	$('#customButton').on('click', (e) => {
+		e.preventDefault();
+	});
+	$('#lookup_field').setupPostcodeLookup({
+		api_key: 'ak_kdh96k99toM85adHd8xyIDxtS67la',
+		output_fields: {
+			line_1: '#first_line',
+			line_2: '#second_line',
+			line_3: '#third_line',
+			post_town: '#post_town',
+			postcode: '#postcode',
+			longitude: '#longitude',
+			latitude: '#latitude',
+			udprn: '#udprn',
+		},
+		button: '#customButton',
 
-  $('#customButton').on('click', (e) => {
-    e.preventDefault();
-  })
-  $('#lookup_field').setupPostcodeLookup({
-    api_key: 'ak_kdh96k99toM85adHd8xyIDxtS67la',
-    output_fields: {
-      line_1: '#first_line',
-      line_2: '#second_line',
-      line_3: '#third_line',
-      post_town: '#post_town',
-      postcode: '#postcode',
-      longitude: '#longitude',
-      latitude: '#latitude',
-      udprn: '#udprn'
-    },
-    button: '#customButton',
+		onSearchCompleted: function (data) {
+			if (data.code === 2000) {
+				// Success
+				$('#successStatus').html('Success!\n' + JSON.stringify(data, 2, 2));
+			} else if (data.code === 4040) {
+				// Postcode does not exist
+				$('#successStatus').html('Postcode does not exist!\n' + JSON.stringify(data, 2, 2));
+			} else {
+				$('#successStatus').html('Some error occurred\n' + JSON.stringify(data, 2, 2));
+			}
+		},
+	});
 
-    onSearchCompleted: function (data) {
-      if (data.code === 2000) {
-        // Success
-        $("#successStatus").html("Success!\n" + JSON.stringify(data, 2, 2));
-      } else if (data.code === 4040) {
-        // Postcode does not exist
-        $("#successStatus").html("Postcode does not exist!\n" + JSON.stringify(data, 2, 2));
-      } else {
-        $("#successStatus").html("Some error occurred\n" + JSON.stringify(data, 2, 2));
-      }
-    }
-
-
-  });
-
-  $('#lookup_field_company').setupPostcodeLookup({
-    api_key: 'ak_kdh96k99toM85adHd8xyIDxtS67la',
-    output_fields: {
-      line_1: '#first_line_company',
-      line_2: '#second_line_company',
-      line_3: '#third_line_company',
-      post_town: '#post_town_company',
-      postcode: '#postcode_company',
-      longitude: '#longitude_company',
-      latitude: '#latitude_company',
-      udprn: '#udprn_company'
-    },
-    button: '#customButton_company',
-    onSearchCompleted: function (data2) {
-      if (data2.code === 2000) {
-        // Success
-        $("#successStatus").html("Success!\n" + JSON.stringify(data2, 2, 2));
-      } else if (data2.code === 4040) {
-        // Postcode does not exist
-        $("#successStatus").html("Postcode does not exist!\n" + JSON.stringify(data2, 2, 2));
-      } else {
-        $("#successStatus").html("Some error occurred\n" + JSON.stringify(data2, 2, 2));
-      }
-    }
-  });
+	$('#lookup_field_company').setupPostcodeLookup({
+		api_key: 'ak_kdh96k99toM85adHd8xyIDxtS67la',
+		output_fields: {
+			line_1: '#first_line_company',
+			line_2: '#second_line_company',
+			line_3: '#third_line_company',
+			post_town: '#post_town_company',
+			postcode: '#postcode_company',
+			longitude: '#longitude_company',
+			latitude: '#latitude_company',
+			udprn: '#udprn_company',
+		},
+		button: '#customButton_company',
+		onSearchCompleted: function (data2) {
+			if (data2.code === 2000) {
+				// Success
+				$('#successStatus').html('Success!\n' + JSON.stringify(data2, 2, 2));
+			} else if (data2.code === 4040) {
+				// Postcode does not exist
+				$('#successStatus').html('Postcode does not exist!\n' + JSON.stringify(data2, 2, 2));
+			} else {
+				$('#successStatus').html('Some error occurred\n' + JSON.stringify(data2, 2, 2));
+			}
+		},
+	});
 }
 
-import "controllers"
+import 'controllers';
 import { data } from 'jquery';
 import googleMap from '../src/google_maps';
 import { map_markers, on_map_divs } from '../src/google_maps';
 
-
 window.initMap = function (...args) {
-  const event = document.createEvent('Events');
-  event.initEvent('google-maps-callback', true, true);
-  event.args = args;
-  window.dispatchEvent(event);
-}
+	const event = document.createEvent('Events');
+	event.initEvent('google-maps-callback', true, true);
+	event.args = args;
+	window.dispatchEvent(event);
+};
